e1e375d86b1d90181c4877d6da240dda1de6be50
whitetulip12 <159236275+whitetulip12@users.noreply.github.com>
2024-02-07 17:27:33 +0530

    Add files via upload

--- statuses.py
+++ statuses.py
@@ -3,6 +3,8 @@ import os
 import fastapi
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from datetime import datetime
+
 
 app = fastapi.FastAPI()
 
@@ -11,17 +13,20 @@ tb = truthbrush.Api(
 
 count = 1
 
-queries= ["Donald Trump", "the", "cricket", "election", "is", "Drake", "Toby",
-          "Jose", "court", "for", "today", "U.S", "United", "america", "breaking"]
+queries = ["Donald Trump", "the", "cricket", "election", "is", "Drake", "Toby",
+           "Jose", "court", "for", "today", "U.S", "United", "america", "breaking"]
+
 
 @app.get("/ping")
 def ping():
     return JSONResponse(status_code=200, content=jsonable_encoder({"message": f"Woke up service"}), media_type="application/json")
 
+
 @app.get("/run")
 def start_brush():
     global count
     for query in queries:
+        print(f"Extracting data for '{query}'")
         response = tb.search(searchtype="statuses", query=query, limit=40)
         for r in response:
             posts = r['statuses']
@@ -29,5 +34,8 @@ def start_brush():
             if len(sorted_list) == 0:
                 break
             for post in sorted_list:
-                print(f"{count}: {post['id']}")
+                if (count % 1000 == 0):
+                    now = datetime.now()
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    print(f"{dt_string}: Extracted {count} docs")
                 count = count+1
