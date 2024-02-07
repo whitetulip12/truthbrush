11d77284df564062c07c82c97d5869b90b7cb86b
whitetulip12 <159236275+whitetulip12@users.noreply.github.com>
2024-02-07 14:01:35 +0530

    Add files via upload

--- statuses.py
+++ statuses.py
@@ -1,15 +1,25 @@
 import truthbrush
 import os
+import fastapi
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
-tb = truthbrush.Api(username=os.environ["USERNAME"], password=os.environ["PASSWORD"])
+app = fastapi.FastAPI()
 
-response = tb.search(searchtype="statuses", query="the", limit=40)
+tb = truthbrush.Api(
+    username=os.environ["USERNAME"], password=os.environ["PASSWORD"])
 
 count = 1
 
-for r in response:
-    posts = r['statuses']
-    sorted_list = sorted(posts, key=lambda x: x['created_at'])
-    for post in sorted_list:
-        print(f"{count}: {post['id']}")
-        count = count+1
+
+@app.get("/")
+def start_brush():
+    global count
+    JSONResponse(status_code=200, content=jsonable_encoder({"message": f"Truthsocial extraction started"}), media_type="application/json")
+    response = tb.search(searchtype="statuses", query="the", limit=40)
+    for r in response:
+        posts = r['statuses']
+        sorted_list = sorted(posts, key=lambda x: x['created_at'])
+        for post in sorted_list:
+            print(f"{count}: {post['id']}")
+            count = count+1
